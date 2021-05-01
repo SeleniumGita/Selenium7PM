7f538027622fab34ac050e78362ae77c75e019e2
SeleniumGita <catchgeets@gmail.com>
2021-04-30 21:44:23 -0500

    modified

--- src/test/java/com/SeleniumScripts/ThirdClass.java
+++ src/test/java/com/SeleniumScripts/ThirdClass.java
@@ -3,7 +3,7 @@ package com.SeleniumScripts;
 public class ThirdClass {
 
 	public static void main(String[] args) {
-		System.out.println("Third class");
+		System.out.println("Third class added");
 		
 	}
 
