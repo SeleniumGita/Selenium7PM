c3b4c1cec62732d8c42d587de110381e45b54715
SeleniumGita <catchgeets@gmail.com>
2021-04-30 21:40:12 -0500

    changes done and new script added

--- src/test/java/com/SeleniumScripts/SecondClass.java
+++ src/test/java/com/SeleniumScripts/SecondClass.java
@@ -4,6 +4,7 @@ public class SecondClass {
 
 	public static void main(String[] args) {
 		System.out.println("My Second class");
+		
 	}
 
 }
--- src/test/java/com/SeleniumScripts/ThirdClass.java
+++ src/test/java/com/SeleniumScripts/ThirdClass.java
@@ -2,4 +2,9 @@ package com.SeleniumScripts;
 
 public class ThirdClass {
 
+	public static void main(String[] args) {
+		System.out.println("Third class");
+		
+	}
+
 }
