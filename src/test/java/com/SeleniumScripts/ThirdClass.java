a499b7c6205d3b885a93e6afafa2743d627cbe95
SeleniumGita <catchgeets@gmail.com>
2021-05-03 14:03:27 -0500

    Changes done to third class

--- src/test/java/com/SeleniumScripts/ThirdClass.java
+++ src/test/java/com/SeleniumScripts/ThirdClass.java
@@ -4,6 +4,8 @@ public class ThirdClass {
 
 	public static void main(String[] args) {
 		System.out.println("third class");
+                System.out.println("third class changes done");
+
 
 	}
 
